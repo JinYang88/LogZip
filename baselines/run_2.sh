0c09802345e9fd4307d6a1ab70b93acb7589e288
Jinyang88 <liujy725@gmail.com>
2019-04-29 01:42:16 +0800

    add run2

--- baselines/run_2.sh
+++ baselines/run_2.sh
@@ -1,7 +1,7 @@
 #！/bin/bash
 
-nohup python run_2.py ../logs/error.log > error.l 2>&1 &
-nohup python run_2.py ../logs/HDFS_2k.log > HDFS_2k.l 2>&1 &
-nohup python run_2.py ../logs/HDFS.log > HDFS.l 2>&1 &
-nohup python run_2.py ../logs/Andriod.log > Andriod.l 2>&1 &
-nohup python run_2.py ../logs/Spark.log > Spark.l 2>&1 &
+nohup python run_2.py --file ../logs/error.log > error.l 2>&1 &
+nohup python run_2.py --file ../logs/HDFS_2k.log > HDFS_2k.l 2>&1 &
+nohup python run_2.py --file ../logs/HDFS.log > HDFS.l 2>&1 &
+nohup python run_2.py --file ../logs/Andriod.log > Andriod.l 2>&1 &
+nohup python run_2.py --file ../logs/Spark.log > Spark.l 2>&1 &
