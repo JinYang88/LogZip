1e814229706548f663eb8de780ebd324f44d3b3a
Jinyang88 <liujy725@gmail.com>
2019-04-28 19:35:23 +0800

    save

--- baselines/run_2.py
+++ baselines/run_2.py
@@ -2,15 +2,17 @@
 
 import argparse
 import os
+import shutil
 parser = argparse.ArgumentParser()
 parser.add_argument('--file', type=str, default="../logs/HDFS_2k.log")
 args = vars(parser.parse_args())
 
 
-
 dirname = os.path.basename(args["file"]).strip(".log")
 output_dir = os.path.join("./2_output", dirname)
-if not os.path.isdir():
-    os.makedirs()
-    
+if not os.path.isdir(output_dir):
+    os.makedirs(output_dir)
+
+os
+os.chdir(output_dir)
 cmd = f"cat {args['file']} | ./Archiver -c --jhistory 10 --buckets 16 --est"
