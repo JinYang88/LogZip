ea4bae7d1c9b49669affbf33c526c3778e4eacfb
Jinyang88 <liujy725@gmail.com>
2019-04-29 14:20:26 +0800

    add run2

--- baselines/run_2.sh
+++ baselines/run_2.sh
@@ -1,7 +1,74 @@
 #！/bin/bash
 
-nohup python run_2.py --file ../logs/error.log > error.l 2>&1 &
-nohup python run_2.py --file ../logs/HDFS_2k.log > HDFS_2k.l 2>&1 &
-nohup python run_2.py --file ../logs/HDFS.log > HDFS.l 2>&1 &
-nohup python run_2.py --file ../logs/Andriod.log > Andriod.l 2>&1 &
-nohup python run_2.py --file ../logs/Spark.log > Spark.l 2>&1 &
+# nohup python run_2.py --file ../logs/error.log > error.l 2>&1 &
+# nohup python run_2.py --file ../logs/HDFS_2k.log > HDFS_2k.l 2>&1 &
+# nohup python run_2.py --file ../logs/HDFS.log > HDFS.l 2>&1 &
+# nohup python run_2.py --file ../logs/Andriod.log > Andriod.l 2>&1 &
+# nohup python run_2.py --file ../logs/Spark.log > Spark.l 2>&1 &
+
+nohup python run_2.py --file ../logs/Thunder_aa > Thunder_aa.l 2>&1 &
+nohup python run_2.py --file ../logs/Thunder_ab > Thunder_ab.l 2>&1 &
+nohup python run_2.py --file ../logs/Thunder_ac > Thunder_ac.l 2>&1 &
+nohup python run_2.py --file ../logs/Thunder_ad > Thunder_ad.l 2>&1 &
+nohup python run_2.py --file ../logs/Thunder_ae > Thunder_ae.l 2>&1 &
+nohup python run_2.py --file ../logs/Thunder_af > Thunder_af.l 2>&1 &
+nohup python run_2.py --file ../logs/Thunder_ag > Thunder_ag.l 2>&1 &
+nohup python run_2.py --file ../logs/Thunder_ah > Thunder_ah.l 2>&1 &
+nohup python run_2.py --file ../logs/Thunder_ai > Thunder_ai.l 2>&1 &
+nohup python run_2.py --file ../logs/Thunder_aj > Thunder_aj.l 2>&1 &
+nohup python run_2.py --file ../logs/Thunder_ak > Thunder_ak.l 2>&1 &
+nohup python run_2.py --file ../logs/Thunder_al > Thunder_al.l 2>&1 &
+nohup python run_2.py --file ../logs/Thunder_am > Thunder_am.l 2>&1 &
+nohup python run_2.py --file ../logs/Thunder_an > Thunder_an.l 2>&1 &
+
+sleep 1h
+
+nohup python run_2.py --file ../logs/Thunder_ao > Thunder_ao.l 2>&1 &
+nohup python run_2.py --file ../logs/Thunder_ap > Thunder_ap.l 2>&1 &
+nohup python run_2.py --file ../logs/Thunder_aq > Thunder_aq.l 2>&1 &
+nohup python run_2.py --file ../logs/Thunder_ar > Thunder_ar.l 2>&1 &
+nohup python run_2.py --file ../logs/Thunder_as > Thunder_as.l 2>&1 &
+nohup python run_2.py --file ../logs/Thunder_at > Thunder_at.l 2>&1 &
+nohup python run_2.py --file ../logs/Thunder_au > Thunder_au.l 2>&1 &
+nohup python run_2.py --file ../logs/Thunder_av > Thunder_av.l 2>&1 &
+nohup python run_2.py --file ../logs/Thunder_aw > Thunder_aw.l 2>&1 &
+nohup python run_2.py --file ../logs/Thunder_ax > Thunder_ax.l 2>&1 &
+nohup python run_2.py --file ../logs/Thunder_ay > Thunder_ay.l 2>&1 &
+nohup python run_2.py --file ../logs/Thunder_az > Thunder_az.l 2>&1 &
+nohup python run_2.py --file ../logs/Thunder_ba > Thunder_ba.l 2>&1 &
+nohup python run_2.py --file ../logs/Thunder_bb > Thunder_bb.l 2>&1 &
+nohup python run_2.py --file ../logs/Thunder_bc > Thunder_bc.l 2>&1 &
+nohup python run_2.py --file ../logs/Thunder_bd > Thunder_bd.l 2>&1 &
+
+sleep 1h
+
+nohup python run_2.py --file ../logs/Windows_aa > Windows_aa.l 2>&1 &
+nohup python run_2.py --file ../logs/Windows_ab > Windows_ab.l 2>&1 &
+nohup python run_2.py --file ../logs/Windows_ac > Windows_ac.l 2>&1 &
+nohup python run_2.py --file ../logs/Windows_ad > Windows_ad.l 2>&1 &
+nohup python run_2.py --file ../logs/Windows_ae > Windows_ae.l 2>&1 &
+nohup python run_2.py --file ../logs/Windows_af > Windows_af.l 2>&1 &
+nohup python run_2.py --file ../logs/Windows_ag > Windows_ag.l 2>&1 &
+nohup python run_2.py --file ../logs/Windows_ah > Windows_ah.l 2>&1 &
+nohup python run_2.py --file ../logs/Windows_ai > Windows_ai.l 2>&1 &
+nohup python run_2.py --file ../logs/Windows_aj > Windows_aj.l 2>&1 &
+nohup python run_2.py --file ../logs/Windows_ak > Windows_ak.l 2>&1 &
+nohup python run_2.py --file ../logs/Windows_al > Windows_al.l 2>&1 &
+nohup python run_2.py --file ../logs/Windows_am > Windows_am.l 2>&1 &
+nohup python run_2.py --file ../logs/Windows_an > Windows_an.l 2>&1 &
+nohup python run_2.py --file ../logs/Windows_ao > Windows_ao.l 2>&1 &
+
+sleep 1h
+
+nohup python run_2.py --file ../logs/Windows_ap > Windows_ap.l 2>&1 &
+nohup python run_2.py --file ../logs/Windows_aq > Windows_aq.l 2>&1 &
+nohup python run_2.py --file ../logs/Windows_ar > Windows_ar.l 2>&1 &
+nohup python run_2.py --file ../logs/Windows_as > Windows_as.l 2>&1 &
+nohup python run_2.py --file ../logs/Windows_at > Windows_at.l 2>&1 &
+nohup python run_2.py --file ../logs/Windows_au > Windows_au.l 2>&1 &
+nohup python run_2.py --file ../logs/Windows_av > Windows_av.l 2>&1 &
+nohup python run_2.py --file ../logs/Windows_aw > Windows_aw.l 2>&1 &
+nohup python run_2.py --file ../logs/Windows_ax > Windows_ax.l 2>&1 &
+nohup python run_2.py --file ../logs/Windows_ay > Windows_ay.l 2>&1 &
+nohup python run_2.py --file ../logs/Windows_az > Windows_az.l 2>&1 &
+nohup python run_2.py --file ../logs/Windows_ba > Windows_ba.l 2>&1 &
