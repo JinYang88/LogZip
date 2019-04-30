d87a54032468ff5dc94f38d0fc993bcda3e671ef
Jinyang88 <liujy725@gmail.com>
2019-04-30 15:56:55 +0800

    add sleep

--- src/experiment/time_vs_nworkers.sh
+++ src/experiment/time_vs_nworkers.sh
@@ -1,44 +1,63 @@
 #！/bin/bash
 
 nohup python time_vs_nworkers.py --postfix 1g --worker 1 --dataset HDFS > HDFS_1g_1.state 2>&1 &
+sleep 2s
 nohup python time_vs_nworkers.py --postfix 1g --worker 2 --dataset HDFS > HDFS_1g_2.state 2>&1 &
+sleep 2s
 nohup python time_vs_nworkers.py --postfix 1g --worker 4 --dataset HDFS > HDFS_1g_4.state 2>&1 &
+sleep 2s
 nohup python time_vs_nworkers.py --postfix 1g --worker 1 --dataset Andriod > Andriod_1g_1.state 2>&1 &
+sleep 2s
 nohup python time_vs_nworkers.py --postfix 1g --worker 2 --dataset Andriod > Andriod_1g_2.state 2>&1 &
+sleep 2s
 nohup python time_vs_nworkers.py --postfix 1g --worker 4 --dataset Andriod > Andriod_1g_4.state 2>&1 &
+sleep 2s
 
 
 nohup python time_vs_nworkers.py --postfix 1g --worker 1 --dataset Spark > Spark_1g_1.state 2>&1 &
+sleep 2s
 nohup python time_vs_nworkers.py --postfix 1g --worker 2 --dataset Spark > Spark_1g_2.state 2>&1 &
+sleep 2s
 nohup python time_vs_nworkers.py --postfix 1g --worker 4 --dataset Spark > Spark_1g_4.state 2>&1 &
+sleep 2s
 nohup python time_vs_nworkers.py --postfix 1g --worker 1 --dataset Thunderbird > Thunderbird_1g_1.state 2>&1 &
+sleep 2s
 nohup python time_vs_nworkers.py --postfix 1g --worker 2 --dataset Thunderbird > Thunderbird_1g_2.state 2>&1 &
+sleep 2s
 nohup python time_vs_nworkers.py --postfix 1g --worker 4 --dataset Thunderbird > Thunderbird_1g_4.state 2>&1 &
 
 sleep 15m
 
 nohup python time_vs_nworkers.py --postfix 1g --worker 1 --dataset Windows > Windows_1g_1.state 2>&1 &
+sleep 2s
 nohup python time_vs_nworkers.py --postfix 1g --worker 2 --dataset Windows > Windows_1g_2.state 2>&1 &
+sleep 2s
 nohup python time_vs_nworkers.py --postfix 1g --worker 4 --dataset Windows > Windows_1g_4.state 2>&1 &
+sleep 2s
 nohup python time_vs_nworkers.py --postfix 1g --worker 8 --dataset HDFS > HDFS_1g_8.state 2>&1 &
+sleep 2s
 nohup python time_vs_nworkers.py --postfix 1g --worker 16 --dataset HDFS > HDFS_1g_16.state 2>&1 &
 
 sleep 15m
 
 nohup python time_vs_nworkers.py --postfix 1g --worker 8 --dataset Andriod > Andriod_1g_8.state 2>&1 &
+sleep 2s
 nohup python time_vs_nworkers.py --postfix 1g --worker 16 --dataset Andriod > Andriod_1g_16.state 2>&1 &
 
 sleep 10m
 
 nohup python time_vs_nworkers.py --postfix 1g --worker 8 --dataset Spark > Spark_1g_8.state 2>&1 &
+sleep 2s
 nohup python time_vs_nworkers.py --postfix 1g --worker 16 --dataset Spark > Spark_1g_16.state 2>&1 &
 
 sleep 10m
 
 nohup python time_vs_nworkers.py --postfix 1g --worker 8 --dataset Thunderbird > Thunderbird_1g_8.state 2>&1 &
+sleep 2s
 nohup python time_vs_nworkers.py --postfix 1g --worker 16 --dataset Thunderbird > Thunderbird_1g_16.state 2>&1 &
 
 sleep 10m
 
 nohup python time_vs_nworkers.py --postfix 1g --worker 8 --dataset Windows > Windows_1g_8.state 2>&1 &
+sleep 2s
 nohup python time_vs_nworkers.py --postfix 1g --worker 16 --dataset Windows > Windows_1g_16.state 2>&1 &
