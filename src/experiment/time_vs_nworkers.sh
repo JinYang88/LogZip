2df84a57c938ddfa645ebb20ab6b26ef9b7bb8f4
Jinyang88 <liujy725@gmail.com>
2019-05-03 17:38:29 +0800

    add statef

--- src/experiment/time_vs_nworkers.sh
+++ src/experiment/time_vs_nworkers.sh
@@ -1,79 +1,79 @@
 #！/bin/bash
 
-rm -rf ./*.state
+rm -rf ./*.statef
 
-nohup python time_vs_nworkers.py --compress_single False --postfix 1g --worker 1 --dataset HDFS > HDFS_1g_1.state 2>&1 &
+nohup python time_vs_nworkers.py --compress_single False --postfix 1g --worker 1 --dataset HDFS > HDFS_1g_1.statef 2>&1 &
 sleep 10s
-nohup python time_vs_nworkers.py --compress_single False --postfix 1g --worker 2 --dataset HDFS > HDFS_1g_2.state 2>&1 &
+nohup python time_vs_nworkers.py --compress_single False --postfix 1g --worker 2 --dataset HDFS > HDFS_1g_2.statef 2>&1 &
 sleep 10s
-nohup python time_vs_nworkers.py --compress_single False --postfix 1g --worker 4 --dataset HDFS > HDFS_1g_4.state 2>&1 &
+nohup python time_vs_nworkers.py --compress_single False --postfix 1g --worker 4 --dataset HDFS > HDFS_1g_4.statef 2>&1 &
 sleep 10s
-nohup python time_vs_nworkers.py --compress_single False --postfix 1g --worker 1 --dataset Andriod > Andriod_1g_1.state 2>&1 &
+nohup python time_vs_nworkers.py --compress_single False --postfix 1g --worker 1 --dataset Andriod > Andriod_1g_1.statef 2>&1 &
 sleep 10s
-nohup python time_vs_nworkers.py --compress_single False --postfix 1g --worker 2 --dataset Andriod > Andriod_1g_2.state 2>&1 &
+nohup python time_vs_nworkers.py --compress_single False --postfix 1g --worker 2 --dataset Andriod > Andriod_1g_2.statef 2>&1 &
 sleep 10s
-nohup python time_vs_nworkers.py --compress_single False --postfix 1g --worker 4 --dataset Andriod > Andriod_1g_4.state 2>&1 &
+nohup python time_vs_nworkers.py --compress_single False --postfix 1g --worker 4 --dataset Andriod > Andriod_1g_4.statef 2>&1 &
 sleep 10s
 
 
-nohup python time_vs_nworkers.py --compress_single False --postfix 1g --worker 1 --dataset Spark > Spark_1g_1.state 2>&1 &
+nohup python time_vs_nworkers.py --compress_single False --postfix 1g --worker 1 --dataset Spark > Spark_1g_1.statef 2>&1 &
 sleep 10s
-nohup python time_vs_nworkers.py --compress_single False --postfix 1g --worker 2 --dataset Spark > Spark_1g_2.state 2>&1 &
+nohup python time_vs_nworkers.py --compress_single False --postfix 1g --worker 2 --dataset Spark > Spark_1g_2.statef 2>&1 &
 sleep 10s
-nohup python time_vs_nworkers.py --compress_single False --postfix 1g --worker 4 --dataset Spark > Spark_1g_4.state 2>&1 &
+nohup python time_vs_nworkers.py --compress_single False --postfix 1g --worker 4 --dataset Spark > Spark_1g_4.statef 2>&1 &
 sleep 10s
-nohup python time_vs_nworkers.py --compress_single False --postfix 1g --worker 1 --dataset Thunderbird > Thunderbird_1g_1.state 2>&1 &
+nohup python time_vs_nworkers.py --compress_single False --postfix 1g --worker 1 --dataset Thunderbird > Thunderbird_1g_1.statef 2>&1 &
 sleep 10s
-nohup python time_vs_nworkers.py --compress_single False --postfix 1g --worker 2 --dataset Thunderbird > Thunderbird_1g_2.state 2>&1 &
+nohup python time_vs_nworkers.py --compress_single False --postfix 1g --worker 2 --dataset Thunderbird > Thunderbird_1g_2.statef 2>&1 &
 sleep 10s
-nohup python time_vs_nworkers.py --compress_single False --postfix 1g --worker 4 --dataset Thunderbird > Thunderbird_1g_4.state 2>&1 &
-
-sleep 15m
-
-nohup python time_vs_nworkers.py --compress_single False --postfix 1g --worker 1 --dataset Windows > Windows_1g_1.state 2>&1 &
-sleep 10s
-nohup python time_vs_nworkers.py --compress_single False --postfix 1g --worker 2 --dataset Windows > Windows_1g_2.state 2>&1 &
-sleep 10s
-nohup python time_vs_nworkers.py --compress_single False --postfix 1g --worker 4 --dataset Windows > Windows_1g_4.state 2>&1 &
-sleep 10s
-nohup python time_vs_nworkers.py --compress_single False --postfix 1g --worker 8 --dataset HDFS > HDFS_1g_8.state 2>&1 &
-sleep 10s
-nohup python time_vs_nworkers.py --compress_single False --postfix 1g --worker 16 --dataset HDFS > HDFS_1g_16.state 2>&1 &
+nohup python time_vs_nworkers.py --compress_single False --postfix 1g --worker 4 --dataset Thunderbird > Thunderbird_1g_4.statef 2>&1 &
 
 sleep 15m
 
-nohup python time_vs_nworkers.py --compress_single False --postfix 1g --worker 8 --dataset Andriod > Andriod_1g_8.state 2>&1 &
+nohup python time_vs_nworkers.py --compress_single False --postfix 1g --worker 1 --dataset Windows > Windows_1g_1.statef 2>&1 &
+sleep 10s
+nohup python time_vs_nworkers.py --compress_single False --postfix 1g --worker 2 --dataset Windows > Windows_1g_2.statef 2>&1 &
+sleep 10s
+nohup python time_vs_nworkers.py --compress_single False --postfix 1g --worker 4 --dataset Windows > Windows_1g_4.statef 2>&1 &
+sleep 10s
+nohup python time_vs_nworkers.py --compress_single False --postfix 1g --worker 8 --dataset HDFS > HDFS_1g_8.statef 2>&1 &
+sleep 10s
+nohup python time_vs_nworkers.py --compress_single False --postfix 1g --worker 16 --dataset HDFS > HDFS_1g_16.statef 2>&1 &
+
+sleep 15m
+
+nohup python time_vs_nworkers.py --compress_single False --postfix 1g --worker 8 --dataset Andriod > Andriod_1g_8.statef 2>&1 &
 sleep 2
-nohup python time_vs_nworkers.py --compress_single False --postfix 1g --worker 16 --dataset Andriod > Andriod_1g_16.state 2>&1 &
+nohup python time_vs_nworkers.py --compress_single False --postfix 1g --worker 16 --dataset Andriod > Andriod_1g_16.statef 2>&1 &
 
 sleep 10m
 
-nohup python time_vs_nworkers.py --compress_single False --postfix 1g --worker 8 --dataset Spark > Spark_1g_8.state 2>&1 &
+nohup python time_vs_nworkers.py --compress_single False --postfix 1g --worker 8 --dataset Spark > Spark_1g_8.statef 2>&1 &
 sleep 10s
-nohup python time_vs_nworkers.py --compress_single False --postfix 1g --worker 16 --dataset Spark > Spark_1g_16.state 2>&1 &
+nohup python time_vs_nworkers.py --compress_single False --postfix 1g --worker 16 --dataset Spark > Spark_1g_16.statef 2>&1 &
 
 sleep 10m
 
-nohup python time_vs_nworkers.py --compress_single False --postfix 1g --worker 8 --dataset Thunderbird > Thunderbird_1g_8.state 2>&1 &
+nohup python time_vs_nworkers.py --compress_single False --postfix 1g --worker 8 --dataset Thunderbird > Thunderbird_1g_8.statef 2>&1 &
 sleep 10s
-nohup python time_vs_nworkers.py --compress_single False --postfix 1g --worker 16 --dataset Thunderbird > Thunderbird_1g_16.state 2>&1 &
+nohup python time_vs_nworkers.py --compress_single False --postfix 1g --worker 16 --dataset Thunderbird > Thunderbird_1g_16.statef 2>&1 &
 
 sleep 10m
 
-nohup python time_vs_nworkers.py --compress_single False --postfix 1g --worker 8 --dataset Windows > Windows_1g_8.state 2>&1 &
+nohup python time_vs_nworkers.py --compress_single False --postfix 1g --worker 8 --dataset Windows > Windows_1g_8.statef 2>&1 &
 sleep 10s
-nohup python time_vs_nworkers.py --compress_single False --postfix 1g --worker 16 --dataset Windows > Windows_1g_16.state 2>&1 &
+nohup python time_vs_nworkers.py --compress_single False --postfix 1g --worker 16 --dataset Windows > Windows_1g_16.statef 2>&1 &
 
 
 sleep 10m
-nohup python time_vs_nworkers.py --compress_single False --postfix 1g --worker 32 --dataset Windows > Windows_1g_32.state 2>&1 &
+nohup python time_vs_nworkers.py --compress_single False --postfix 1g --worker 32 --dataset Windows > Windows_1g_32.statef 2>&1 &
 sleep 10m
-nohup python time_vs_nworkers.py --compress_single False --postfix 1g --worker 32 --dataset Thunderbird > Thunderbird_1g_32.state 2>&1 &
+nohup python time_vs_nworkers.py --compress_single False --postfix 1g --worker 32 --dataset Thunderbird > Thunderbird_1g_32.statef 2>&1 &
 sleep 10m
-nohup python time_vs_nworkers.py --compress_single False --postfix 1g --worker 32 --dataset Andriod > Andriod_1g_32.state 2>&1 &
+nohup python time_vs_nworkers.py --compress_single False --postfix 1g --worker 32 --dataset Andriod > Andriod_1g_32.statef 2>&1 &
 sleep 10m
-nohup python time_vs_nworkers.py --compress_single False --postfix 1g --worker 32 --dataset HDFS > HDFS_1g_32.state 2>&1 &
+nohup python time_vs_nworkers.py --compress_single False --postfix 1g --worker 32 --dataset HDFS > HDFS_1g_32.statef 2>&1 &
 sleep 10m
-nohup python time_vs_nworkers.py --compress_single False --postfix 1g --worker 32 --dataset Spark > Spark_1g_32.state 2>&1 &
+nohup python time_vs_nworkers.py --compress_single False --postfix 1g --worker 32 --dataset Spark > Spark_1g_32.statef 2>&1 &
 sleep 10m
 
