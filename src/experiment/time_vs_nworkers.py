76cb0e15ca2d1f1b34df39eb63e349ca74f4006a
Jinyang88 <liujy725@gmail.com>
2019-05-03 16:03:17 +0800

    add cs

--- src/experiment/time_vs_nworkers.py
+++ src/experiment/time_vs_nworkers.py
@@ -25,6 +25,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--worker", type=int, default="1")
 parser.add_argument("--postfix", type=str, default="2k")
 parser.add_argument("--dataset", type=str, default="HDFS")
+parser.add_argument('--compress_single', type=str, default="False")
 args = vars(parser.parse_args())
 del parser
 
@@ -33,7 +34,8 @@ print(args)
 postfix = args["postfix"]
 n_workers = args["worker"]  # Number of processes.
 dataset = args["dataset"]
+compress_single = args["compress_single"]
 
 logfile = os.path.join("../../logs", f"{dataset}_{postfix}.log")
 logzip(logfile, outdir, log_format_dict[dataset], n_workers=n_workers,
-             level=level, top_event=top_event, kernel=kernel, report_file=report_file)
+             level=level, top_event=top_event, kernel=kernel, report_file=report_file, compress_single=compress_single)
