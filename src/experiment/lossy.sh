e467039afb0071ab58ae853d0bad701c2101dcac
Jinyang88 <liujy725@gmail.com>
2019-05-13 15:51:49 +0800

    add lossy

--- src/experiment/lossy.sh
+++ src/experiment/lossy.sh
@@ -1,8 +1,8 @@
 #！/bin/bash
 
 
-python lossy.py --compress_single True --postfix 1g --worker 4 --dataset HDFS > HDFS_1g_lossy.statet 2>&1
-python lossy.py --compress_single True --postfix 1g --worker 4 --dataset Spark > Spark_1g_lossy.statet 2>&1
-python lossy.py --compress_single True --postfix 1g --worker 4 --dataset Andriod > Andriod.statet 2>&1
-python lossy.py --compress_single True --postfix 1g --worker 4 --dataset Windows > Windows_1g_lossy.statet 2>&1
-python lossy.py --compress_single True --postfix 1g --worker 4 --dataset Thunderbird > Thunderbird_1g_lossy.statet 2>&1
+python lossy.py --compress_single True --postfix 1g --worker 4 --dataset HDFS > HDFS_1g_lossy.statet 2>&1 &
+python lossy.py --compress_single True --postfix 1g --worker 4 --dataset Spark > Spark_1g_lossy.statet 2>&1 &
+python lossy.py --compress_single True --postfix 1g --worker 4 --dataset Andriod > Andriod.statet 2>&1 &
+python lossy.py --compress_single True --postfix 1g --worker 4 --dataset Windows > Windows_1g_lossy.statet 2>&1 &
+python lossy.py --compress_single True --postfix 1g --worker 4 --dataset Thunderbird > Thunderbird_1g_lossy.statet 2>&1 &
